d788c0c2314232f1d87ca122ebed1e3eacebdb9f
square <fxz@huizuche.com>
2016-07-15 21:02:52 +0800

    模块化

--- app.js
+++ app.js
@@ -2,100 +2,114 @@
  * Created by fangxianzheng on 2016/7/6.
  */
 
-/*
 
-;(function(win){
+;(function(win, IScroll){
     var FDatetime = function(opts){
-        this.startTime = opts.startTime
-        this.endTime = opts.endTime
-
+        this.startDate = new Date(opts.startTime);
+        this.endDate = new Date(opts.endTime);
+        this.defaultDate = new Date(opts.defaultTime);
+        this.inseritLi()
     }
 
     FDatetime.prototype = {
+        createYearLi:function(){
+            var self = this;
+            var startTime = self.startDate.getTime();
+            var endTime =  self.endDate.getTime();
+            var millisecondSpan = endTime - startTime;
+            var daySpan = Math.ceil(millisecondSpan /1000/3600/24);
+            var dayMillisecond = 3600 * 24 * 1000;
+            var dayTime = startTime - dayMillisecond;
+            var html = '<li></li>\n<li></li>\n<li></li>';
+            for(var i=0; i<daySpan;i++){
+                dayTime += dayMillisecond;
+                html += '<li data-date="'+ calculate(dayTime, 'normal') +'">' + calculate(dayTime, 'zh') + '</li>\n'
+            }
+            html = html +'<li></li>\n<li></li>\n<li></li>';
+            return html
+        },
+        createHourLi:function(){
+            var self = this;
+            var j = 1;
+            var html = '<li></li>\n<li></li>\n<li></li>';
+            for(var i = 0; i< 24; i++){
+                j++
+                i = i<10 ? '0' + i : i;
+                html += '<li>' + i + '</li>'
+            }
+            console.log(html)
+        },
+        inseritLi:function(){
+            var self = this;
+            $('#fd-year ul').html(self.createLi())
+            var yearScroll = new IScroll('#fd-year',{
+                snap: 'li'
+
+            })
+        }
+    }
+
+    /********私有方法************************************/
+    function calculate(dayTime, type){
+        var dayDate = new Date(dayTime);
+        var toLocaleDateString = dayDate.toLocaleDateString();
+        var getHours = dayDate.getHours();
+        var getMinutes = dayDate.getMinutes();
+        var getSeconds = dayDate.getSeconds();
+
+        var month = dayDate.getMonth();
+        var date = dayDate.getDate();
+        var day = dayDate.getDay();
+        month = month + 1;
+        month = month < 10 ? '0' + month : month;
+        date = date < 10 ? '0' + date : date;
+        switch (day){
+            case 0:
+                day = '周日';
+                break;
+            case 1:
+                day = '周一';
+                break;
+            case 2:
+                day = '周二';
+                break;
+            case 3:
+                day = '周三';
+                break;
+            case 4:
+                day = '周四';
+                break;
+            case 5:
+                day = '周五';
+                break;
+            case 6:
+                day = '周六';
+                break
+        }
+        if(/zh/.test(type)){
+            return month + '月' + date + '日 ' + day;
+        }else if(/normal/.test(type)){
+            return toLocaleDateString + ' ' + getHours + ':' + getMinutes + ':' +  getSeconds;
+        }
 
     }
 
-    /!********工具函数************************************!/
 
-    function getTime(dateStr){
-        return new Date(dateStr).getTime();
-    }
 
     win.FDatetime = FDatetime;
-})(window,undefined)
+})(window,IScroll,undefined)
 
 
 var o = new FDatetime({
-    startTime:'2016-8-8',
+    startTime:'2017-8-8',
     endTime:'2017-9-3',
     defaultTime:'2016-9-10 10:00:00'
 })
-*/
 
 
 
 
 
-var now = new Date();
-var endDate = new Date('2017-07-16 10:20:59');
-var nowTime = now.getTime();
-var endTime = endDate.getTime();
-var millisecondSpan = endTime - nowTime;
 
-var daySpan = Math.ceil(millisecondSpan /1000/3600/24);
-
-var html = '';
-var dayMillisecond = 3600 * 24 * 1000;
-var dayTime = nowTime - dayMillisecond;
-for(var i=0; i<daySpan;i++){
-    dayTime += dayMillisecond;
-    html += '<li>' + calculate(dayTime) + '</li>\n'
-}
-$('#fd-year ul').html(html)
-
-
-function calculate(dayTime){
-    var dayDate = new Date(dayTime);
-
-    var month = dayDate.getMonth();
-    var date = dayDate.getDate();
-    var day = dayDate.getDay();
-    month = month + 1;
-    month = month < 10 ? '0' + month : month;
-    date = date < 10 ? '0' + date : date;
-    switch (day){
-        case 0:
-            day = '周日'
-            break
-        case 1:
-            day = '周一'
-            break
-        case 2:
-            day = '周二'
-            break
-        case 3:
-            day = '周三'
-            break
-        case 4:
-            day = '周四'
-            break
-        case 5:
-            day = '周五'
-            break
-        case 6:
-            day = '周六'
-            break
-    }
-
-    return month + '月' + date + '日 ' + day;
-}
-
-
-
-
-var yearScroll = new IScroll('#fd-year',{
-    snap: 'li'
-
-})
 
 
